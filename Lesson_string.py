e1c9ba87290a8f49563be5ee2d498d49cb3bbe62
Evgen Rud <evgeniy.rud86@gmail.com>
2024-03-14 15:00:11 +0200

    ver 4.1

--- Lesson_string.py
+++ Lesson_string.py
@@ -1,57 +1,47 @@
 # Задача 1 - Написати програму, в якій необхідно видалити всі вказані літери з стрінги(string)
-print("Введіть текст")
-line_1 = input()
-print("Введіть літеру")
-letter_need_to_delete = input()
+line_1 = input("Enter text: ")
+letter_need_to_delete = input("Enter letter: ")
 final_text_1 = line_1.replace(letter_need_to_delete,"")
-print(final_text_1)
+print(f'Result is {final_text_1}')
 
 # Написати програму, яка візьме string і в кожному слові замінить першу літеру на велику.
 
-print("Введіть текст")
-line_2 = input()
+line_2 = input("Enter text: ")
 final_text_2 = str.title(line_2)
-print(final_text_2)
+print(f' Final text is {final_text_2}')
 
 #Напишіть програму яка перевертає стрінгу
 
-print("Введіть текст, який потрібно перевенути")
-line_3 = input()
+line_3 = input('Enter text for changing: ')
 final_text_3 = line_3[::-1]
-print(final_text_3)
+print(f'Changing text is {final_text_3}')
 
 # Напишіть програму яка приймає на вхід 2 стрінги та порівнює їх
 
-print("Введіть вихідний текст")
-line_4 = input()
-print("Введіть текст, який потрібно порівняти")
-line_4_compare = input()
+line_4 = input('Enter text: ')
+line_4_compare = input('Enter text for compare :')
 if line_4 == line_4_compare:
-    print("Тексти однакові")
+    print("Texts are equals")
 else:
-    print("Тексти відрізняються")
+    print("Texts are not equals")
 
 # Напишіть програму яка візьме стрінг та повторить її задану кількість раз.
 
-print("Введіть текст")
-line_5 = input()
-print("Введіть кількість повторів")
-koef_5 = int(input())
-print(line_5*koef_5)
+line_5 = input('Enter text: ')
+koef_5 = int(input('Enter count repeats'))
+print(f'Final text is : {line_5*koef_5}')
 
 # Написати програму для підрахунку конвертації валюти
 
-print("Введіть сумму")
-sum_6 = float(input())
-print("Введіть валюту суми (UAH/USD/EUR)")
-currency_1 = input()
+sum_6 = float(input('Enter amount :'))
+currency_1 = input('Enter currency (UAH/USD/EUR) :')
 if currency_1 != 'UAH' and currency_1 != 'USD' and currency_1 != 'EUR':
-    print("Валюта введена некорректно")
+    print("Currency is incorrect")
 else:
     print("Введіть валюту конвертації (UAH/USD/EUR)")
     currency_2 = input()
     if currency_2 != "UAH" and currency_2 != "USD" and currency_2 != "EUR":
-        print("Валюта введена некорректно")
+        print("Currency is incorrect")
     else:
         if currency_1 == 'UAH':
             koef_1 = 1
@@ -66,4 +56,4 @@ else:
         else:
             koef_2 = 40
         sum_final = round(sum_6*koef_1/koef_2,2)
-        print(sum_final,' ',currency_2)
+        print(f'Final sum is : {sum_final} {currency_2}')
